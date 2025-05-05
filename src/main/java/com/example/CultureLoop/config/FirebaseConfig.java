500738ab3eebc45c2d780b9f96ee1fc357d97ce3
hyeleenotharry <dlgpgl1029@gmail.com>
2025-05-05 14:58:53 +0900

    classpath

--- src/main/java/com/example/CultureLoop/config/FirebaseConfig.java
+++ src/main/java/com/example/CultureLoop/config/FirebaseConfig.java
@@ -8,10 +8,12 @@ import jakarta.annotation.PostConstruct;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.ClassPathResource;
 
 @Slf4j
 @Configuration
@@ -19,8 +21,8 @@ public class FirebaseConfig {
     @PostConstruct
     public void init() throws IOException {
         log.info("Initializing firebase");
-        FileInputStream serviceAccount =
-                new FileInputStream("src/main/resources/firebase.json");
+        File file = new ClassPathResource("firebase.json").getFile();
+        FileInputStream serviceAccount = new FileInputStream(file);
 
         FirebaseOptions options = FirebaseOptions.builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
