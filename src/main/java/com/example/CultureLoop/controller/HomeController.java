bd0b9a0f6feec6a09a6db1207eb347b4413dc871
hyeleenotharry <dlgpgl1029@gmail.com>
2025-05-16 17:04:08 +0900

    home

--- src/main/java/com/example/CultureLoop/controller/HomeController.java
+++ src/main/java/com/example/CultureLoop/controller/HomeController.java
@@ -1,7 +1,9 @@
 package com.example.CultureLoop.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class HomeController {
     @GetMapping("/")
     public String healthCheck() {
